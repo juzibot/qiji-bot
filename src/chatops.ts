61cd3ba4b3c0db4ce724299da516931416a95cb7
Huan LI <zixia@zixia.net>
2019-12-23 14:49:01 +0800

    Enable ChatOps

---
+++ src/chatops.ts
@@ -0,0 +1,31 @@
+import {
+  Room,
+  Wechaty,
+}             from 'wechaty'
+
+import {
+  log,
+}                   from './config'
+
+const CHATOPS_ROOM_TOPIC  = 'ChatOps - Qiji BOT'
+const CHATOPS_ROOM_ID     = '"19995279701@chatroom"'
+
+let room: Room
+
+export async function chatops (
+  bot: Wechaty,
+  text: string,
+): Promise<void> {
+  log.info('chatops', 'chatops(%s)', text)
+
+  if (!room) {
+    let tryRoom = await bot.Room.find({ topic: CHATOPS_ROOM_TOPIC })
+    if (!tryRoom) {
+      tryRoom = bot.Room.load(CHATOPS_ROOM_ID)
+    }
+
+    room = tryRoom
+  }
+
+  await room.say(text)
+}
