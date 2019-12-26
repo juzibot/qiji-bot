7c873a3c032cf9adf8813243fd12176c68a6a61b
rickyyin98 <rickyyin98@163.com>
2019-12-27 02:57:53 +0800

    Update Saying

--- src/managers/friendship-manager.ts
+++ src/managers/friendship-manager.ts
@@ -1,7 +1,7 @@
 import { Friendship } from 'wechaty'
 
 const NEW_FRIEND_GREETING = [
-  '你好！', '这里是奇绩创坛！',
+  '您好！我是奇绩创坛的尹伯昊[Hey]之后报名、面试的相关问题都可以问我。我们春季创业营会在2020年1月中旬开始邀请项目面试，越早填写报名表（apply.miracleplus.com）我们的团队会越早看到，如果您方便的话，可以最近准备填写完整和提交报名表啦。', '关注到您注册了我们春季创业营的报名表但是还没有填写完成，您具体是因为什么原因还没填写完报名表呀？',
 ]
 
 export class FriendshipManager {
