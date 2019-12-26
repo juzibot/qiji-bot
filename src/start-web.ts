ac81c44db53dcecd4bf22c65bb116ca0cbac6662
rickyyin98 <rickyyin98@163.com>
2019-12-26 17:14:09 +0800

    Update start-web.ts

--- src/start-web.ts
+++ src/start-web.ts
@@ -1,4 +1,4 @@
-import Hapi    from '@hapi/hapi'
+import Hapi, { Request, ResponseToolkit }    from '@hapi/hapi'
 import {
   Wechaty,
 }               from 'wechaty'
@@ -8,6 +8,34 @@ import {
   PORT,
   VERSION,
 }             from './config'
+import { chatops } from './chatops'
+
+let wechaty: Wechaty
+
+async function chatopsHandler (request: Request, response: ResponseToolkit) {
+  log.info('startWeb', 'chatopsHandler()')
+
+  const payload: {
+    chatops: string,
+  } = request.payload as any
+
+  await chatops(wechaty, payload.chatops)
+
+  return response.redirect('/')
+}
+
+export async function githubWebhookHandler (
+  request: Request,
+  response: ResponseToolkit,
+) {
+  log.info('startWeb', 'githubWebhookHandler()')
+
+  const payload = request.payload as any
+
+  log.verbose(JSON.stringify(payload))
+
+  return response.response()
+}
 
 export async function startWeb (bot: Wechaty): Promise<void> {
   log.verbose('startWeb', 'startWeb(%s)', bot)
@@ -15,14 +43,26 @@ export async function startWeb (bot: Wechaty): Promise<void> {
   let qrcodeValue : undefined | string
   let userName    : undefined | string
 
+  wechaty = bot
+
   const server =  new Hapi.Server({
     port: PORT,
   })
 
-  const handler = () => {
+  const FORM_HTML = `
+    <form action="/chatops/" method="post">
+      <label for="chatops">ChatOps: </label>
+      <input id="chatops" type="text" name="chatops" value="Hello, BOT5.">
+      <input type="submit" value="ChatOps">
+    </form>
+  `
+  const handler = async () => {
+    let html
+
     if (qrcodeValue) {
-      const html = [
-        `<h1>Heroku Wechaty Getting Started v${VERSION}</h1>`,
+
+      html = [
+        `<h1>BOT5 v${VERSION}</h1>`,
         'Scan QR Code: <br />',
         qrcodeValue + '<br />',
         '<a href="http://goqr.me/" target="_blank">http://goqr.me/</a><br />',
@@ -32,18 +72,41 @@ export async function startWeb (bot: Wechaty): Promise<void> {
         encodeURIComponent(qrcodeValue),
         '">',
       ].join('')
-      return html
+
     } else if (userName) {
-      return `Heroku Wechaty Getting Started v${VERSION} User ${userName} logined`
+      let roomList = await bot.Room.findAll()
+      let roomHtml = `The rooms I have joined are as follows: <ol>`
+      for (let room of roomList) {
+        const topic = await room.topic()
+        const roomId = room.id
+        roomHtml = roomHtml + `<li> ${topic} / ${roomId} </li>\n`
+      }
+      roomHtml = roomHtml + `</ol>`
+
+      html = [
+        `<p> BOT5 v${VERSION} User ${userName} logined. </p>`,
+        FORM_HTML,
+        roomHtml,
+      ].join('')
+
     } else {
-      return `Heroku Wechaty Getting Started v${VERSION} Hello, come back later please.`
+
+      html = `BOT5 v${VERSION} Hello, come back later please.`
+
     }
+    return html
   }
 
   server.route({
     handler,
     method : 'GET',
     path   : '/',
+  })
+
+  server.route({
+    handler: chatopsHandler,
+    method : 'POST',
+    path   : '/chatops/',
   })
 
   bot.on('scan', qrcode => {
