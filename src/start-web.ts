3adcbf5f3aa41608d58dce19287e15c062c4a72f
rickyyin98 <rickyyin98@163.com>
2020-01-01 16:23:04 +0800

    Update start-web.ts

--- src/start-web.ts
+++ src/start-web.ts
@@ -71,14 +71,14 @@ export async function startWeb (
       <input type="submit" value="ChatOps">
     </form>
   `
-  const getMessageHtml = (mes: Message, saying: string, abb: string) => {
+  const getMessageHtml = (mes: Message, saying: string, abb: string, color:string) => {
     const from = mes.from()
     // <label for="sendmes">${saying}</label>
     return `
       <form action="/sendmes/" method="post" style="display:inline">
-        <input id="${mes.id}" type="hidden" name="text" value="${saying}" style="width:200px,height:70px">
-        <input id="${mes.id}" type="hidden" name="toId" value="${(from && from.id) || ''}" style="width:200px,height:70px">
-        <input type="submit" value="${abb}" style="width:200px,height:70px">
+        <input id="${mes.id}" type="hidden" name="text" value="${saying}" >
+        <input id="${mes.id}" type="hidden" name="toId" value="${(from && from.id) || ''}">
+        <input type="submit" value="${abb}" style="width:200px;height:50px;background-color: #${color};border-radius:5px">
       </form>
     `
   }
@@ -106,9 +106,9 @@ export async function startWeb (
         if (mes.from()?.name() !== userName) {
           const what = mes.text()
           const who = mes.from()?.name()
-          const NewHTML = getMessageHtml(mes, '我们对于项目是没有要求的，以我们刚刚结束的第一期创业营为例，其中既有像脑机接口这样的硬科技项目、也有像鱼塘物联网设备这样的技术落地项目、还有像中老年人帮子女相亲的社区这样的C端项目。', '对项目没有要求')
-          const NextHTML = getMessageHtml(mes, '我们第一期的很多项目实际上都很优秀，但是因为我们第一期的规模有限没有办法邀请所有项目进入创业营，而我们这一期将会扩招，相信您的项目有机会成为其中之一。', '为什么上一期落选')
-          const ThirdHTML = getMessageHtml(mes, '您可以介绍一下您的项目嘛？我们很期待触达更多的创业者并帮助他们融资。', '邀请介绍项目')
+          const NewHTML = getMessageHtml(mes, '我们对于项目是没有要求的，以我们刚刚结束的第一期创业营为例，其中既有像脑机接口这样的硬科技项目、也有像鱼塘物联网设备这样的技术落地项目、还有像中老年人帮子女相亲的社区这样的C端项目。', '对项目没有要求', '3f3f3f')
+          const NextHTML = getMessageHtml(mes, '我们第一期的很多项目实际上都很优秀，但是因为我们第一期的规模有限没有办法邀请所有项目进入创业营，而我们这一期将会扩招，相信您的项目有机会成为其中之一。', '为什么上一期落选', '3f3f3f')
+          const ThirdHTML = getMessageHtml(mes, '您可以介绍一下您的项目嘛？我们很期待触达更多的创业者并帮助他们融资。', '邀请介绍项目', '3f3f3f')
           MessageHtml = MessageHtml + `<li> ${who} / ${what} </li>\n` + NewHTML + NextHTML + ThirdHTML
         }
         MessageHtml = MessageHtml + `</ol>`
